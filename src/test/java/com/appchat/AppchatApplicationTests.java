27e0f800b69b1eaea8101243dabe2dd9b1709f63
Cuong2k2 <xinloinhe23@gmail.com>
2023-07-24 18:38:08 +0700

    fist

---
+++ src/test/java/com/appchat/AppchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
